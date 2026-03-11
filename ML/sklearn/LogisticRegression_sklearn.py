446735024b06ee1382368d8f8866268645942a82
calmnight <max61870@gmail.com>
2026-03-11 14:35:11 +0800

    20260311

---
+++ ML/sklearn/LogisticRegression_sklearn.py
@@ -0,0 +1,128 @@
+### Load Iris dataset from scikit-learn
+from sklearn import datasets
+import numpy as np
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+print('Class labels:', np.unique(y))
+
+
+### Splitting data into 70% training and 30% test data
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+
+### Feature scaling (Standardization)
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+
+### Training a Logistic Regression via scikit-learn
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
+
+
+### Model predict
+y_pred = lr.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+
+
+### Model evaluation
+from sklearn.metrics import accuracy_score
+
+print('Accuracy: %.4f' % accuracy_score(y_test, y_pred))
+
+### Visualization
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import warnings
+
+
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.6, 
+                    facecolors=cmap(idx),
+                    edgecolors='black',
+                    marker=markers[idx], 
+                    label=f'class {cl}')
+
+    # highlight test samples
+    if test_idx is not None:
+        # plot all samples
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    facecolors='none',
+                    alpha=1.0,
+                    edgecolor='black',
+                    linewidths=2.5,
+                    marker='o',
+                    s=120, label='test set')
+        
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=lr, test_idx=range(len(X_train_std), len(X_combined_std)))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+# plt.savefig('./figures/iris_perceptron_scikit.png', dpi=300)
+plt.show()
+
+weights, params = [], []
+for c in np.arange(-5., 5.):
+    lr = LogisticRegression(penalty='l2', C=10.**c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0],
+         label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--',
+         label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
+# plt.savefig('./figures/regression_path.png', dpi=300)
+plt.show()
